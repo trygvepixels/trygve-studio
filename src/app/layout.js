8e6d1d00f983ace5222d94bd8dc925b13e06d5a4
harshrao26 <harshurao058@gmail.com>
2025-10-05 19:47:12 +0530

    GTM Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -160,6 +160,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TVF6BFPQ');
+          `}
+        </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -184,6 +193,10 @@ export default function RootLayout({ children }) {
         </noscript>
       </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVF6BFPQ"
+          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+        </noscript>
         <Header />
 
         <div className="md:pt-24 pt-32">{children}</div>
